4958f74a5e6339c2ea478fa52d3a262f8f993ad5
Thibault Poncelet <thibault.poncelet@lt-servers.be>
2011-10-14 11:21:20 +0200

    INIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rphenomenal::Application.config.secret_token = 'e55d0ec00a580d2170a5436f1421b023d6c92c03dd8f15bd480bdc53bd776c37bdc4beed6a2c442f4dad35654e6adbdbcbbed5994a8ba7d0aba1eb0f16ef1588'
