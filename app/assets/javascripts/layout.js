e20b5566f034e6f269e8a3de3063768b790a9116
Thibault Poncelet <thibault.poncelet@lt-servers.be>
2011-11-01 00:53:52 +0100

    added mysql

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -11,18 +11,5 @@ $(document).ready(function() {
     $(elem).children(".caption").html(content);
   });
   slide(0);
-  
-  $('textarea').tinymce({
-  // Location of TinyMCE script
-    plugins : 'cesam', // - tells TinyMCE to skip the loading of the plugin
-    mode : "textareas",
-    theme : "advanced",
-    theme_advanced_buttons1 : "bold,italic,underline,forecolor,separator,strikethrough,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,undo,redo,link,unlink,separator,uservars",
-    theme_advanced_buttons2 : "",
-    theme_advanced_buttons3 : "",
-    theme_advanced_toolbar_location : "top",
-    theme_advanced_toolbar_align : "left",
-    theme_advanced_statusbar_location : "bottom"
-  });
 })
 
