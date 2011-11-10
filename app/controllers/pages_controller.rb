6dcedd670842366bc67a5f6cd99f154483b4363a
Thibault Poncelet <thibault.poncelet@lt-servers.be>
2011-11-10 09:50:45 +0100

    rss feed comit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,18 @@
 class PagesController < ApplicationController
   def home
     @title="Welcome on phenomenal-gem.com"
-    if Rails.env.production? 
-      g = Git.open ('/var/www/rphenomenal')
-      c = g.log.first
-      @commit ="#{l c.date} - #{c.author.name} - #{c.message}"
-    else
-      @commit = "Last commit dev mode"
-    end
+    # fetching a single feed
+    feed = Feedzirra::Feed.fetch_and_parse("http://redmine.phenomenal-gem.com/projects/phenomenal/repository/revisions.atom?key=5769b0cc85a55d8dbed695fff0068ebcb78582be")
+
+    entry = feed.entries.first
+    entry.title      # => "Ruby Http Client Library Performance"
+    entry.url        # => "http://www.pauldix.net/2009/01/ruby-http-client-library-performance.html"
+    entry.author     # => "Paul Dix"
+    entry.summary    # => "..."
+    entry.content    # => "..."
+    entry.published  # => Thu Jan 29 17:00:19 UTC 2009 # it's a Time object
+    entry.categories # => ["...", "..."]
+    @commit ="#{entry.title} - #{entry.published} - #{entry.author}"
   end
   
   def project
