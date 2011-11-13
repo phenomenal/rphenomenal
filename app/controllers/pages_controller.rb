23f40f7d3e0836643395e526818d749fba80154b
Thibault Poncelet <thibault.poncelet@lt-servers.be>
2011-11-13 20:30:08 +0100

    added pheno-rails latest revision

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
   def home
     @title="Welcome on phenomenal-gem.com"
-    # fetching a single feed
+    # fetching phenomenal commit
     feed = Feedzirra::Feed.fetch_and_parse("http://redmine.phenomenal-gem.com/projects/phenomenal/repository/revisions.atom?key=5769b0cc85a55d8dbed695fff0068ebcb78582be")
     entry = feed.entries.first
     @commit_phenomenal="#{entry.title} - #{entry.published} - #{entry.author}"
+    # fetching phenomenal-rails commit 
+    feed = Feedzirra::Feed.fetch_and_parse("http://redmine.phenomenal-gem.com/projects/phenomenal-rails/repository/revisions.atom?key=5769b0cc85a55d8dbed695fff0068ebcb78582be")
+    entry = feed.entries.first
+    @commit_phenomenal_rails="#{entry.title} - #{entry.published} - #{entry.author}"
   end
   
   def project
